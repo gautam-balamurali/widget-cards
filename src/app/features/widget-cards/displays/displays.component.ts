819bd989526a69eca338681a132e2da066519af5
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-03-10 11:30:34 +0530

    feat: API integration in displays widget

--- src/app/features/widget-cards/displays/displays.component.ts
+++ src/app/features/widget-cards/displays/displays.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+interface Displays {
+  name: string;
+  on_time: string;
+  total: number;
+  value: number;
+}
 
 @Component({
   selector: 'app-displays',
   templateUrl: './displays.component.html',
-  styleUrls: ['./displays.component.scss']
+  styleUrls: ['./displays.component.scss'],
 })
 export class DisplaysComponent implements OnInit {
-
-  constructor() { }
+  @Input() displayData: Displays;
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.displayData);
   }
-
 }
